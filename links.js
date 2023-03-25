d404dbc589d61f60d4e07353463436be62c66e5a
Aqdas Ahmad Khan <aqdasak@gmail.com>
2023-03-25 23:06:51 +0530

    Added link to user upload folder

--- links.js
+++ links.js
@@ -6,4 +6,6 @@
 const links = {
   "https://script.google.com/macros/s/AKfycbyf4JZ9PFSPGXlZRm5zTM-c8_6jnR1YjqW70seeDJ43o1faqqyLLlHpmC1UbkR5ekmqCQ/exec":
     "Upload Files",
+  "https://drive.google.com/drive/folders/1z_9NWUReRiJzMaEzSpyy-1YsWmnC8iKo":
+    "Uploaded Files",
 };
